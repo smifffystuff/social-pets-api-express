5ecb8793df9e7459ce9123ddb41164cba2f3bea9
Martin Smith <martin.smifff@gmail.com>
2019-07-16 12:41:51 +0100

    Created a very basic express app, added nodemon to dev dependancies and alsoy created a dev script

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 require("dotenv").config();
+const express = require("express");
 
-console.log(process.env.SECRET);
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
